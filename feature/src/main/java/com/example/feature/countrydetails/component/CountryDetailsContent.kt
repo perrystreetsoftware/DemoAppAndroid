5059e7125048d682f0edfffa5bf354cfb4ffca8d
Eric Silverberg <eric@perrystreet.com>
2022-10-24 23:36:07 -0400

    Remove files

--- feature/src/main/java/com/example/feature/countrydetails/component/CountryDetailsContent.kt
+++ feature/src/main/java/com/example/feature/countrydetails/component/CountryDetailsContent.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.feature.countryselecting.CountrySelectingPage
-import com.example.feature.countryselecting.CountrySelectingUIState
 import com.example.viewmodels.CountrySelectingViewModel
 
 @Composable
--- feature/src/main/java/com/example/feature/countryselecting/CountrySelectingUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.feature.countryselecting
-
-import com.example.domainmodels.Continent
-import com.example.viewmodels.CountrySelectingViewModel
-
-data class CountrySelectingUIState(
-    val continents: List<Continent>,
-    val viewModelState: CountrySelectingViewModel.UiState
-)
