bfa1fcfbe189135e0b7eb7a5b8605667ae1eb573
oscar <oscar@perrystreet.com>
2022-11-18 11:41:55 +0100

    Map errors to UI component

--- feature/src/main/java/com/example/feature/countrylist/CountryListAdapter.kt
+++ feature/src/main/java/com/example/feature/countrylist/CountryListAdapter.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.rxjava3.subscribeAsState
 import androidx.compose.ui.res.stringResource
 import com.example.domainmodels.Country
 import com.example.errors.CountryListError
-import com.example.feature.extensions.asUIError
+import com.example.features.R
 import com.example.viewmodels.CountryListViewModel
 import org.koin.androidx.compose.getViewModel
 
@@ -24,24 +24,54 @@ fun CountryListAdapter(
         onRefreshTapped = { viewModel.onRefreshTapped() },
     )
 
-    ErrorDialog(
-        state = state.error,
-        onDismiss = { viewModel.onErrorDismissed() }
-    )
+    if (state.error != null) {
+        AlertDialog(
+            error = state.error!!,
+            onDismiss = { viewModel.onErrorDismissed() }
+        )
+    }
 }
 
+
 @Composable
-fun ErrorDialog(state: CountryListError?, onDismiss: () -> Unit) {
-    if (state != null) {
-        (state!!.asUIError()).let { uiError ->
+fun AlertDialog(error: CountryListError, onDismiss: () -> Unit) {
+    return when(error) {
+        is CountryListError.Forbidden -> {
             AlertDialog(
                 onDismissRequest = onDismiss,
-                title = { Text(text = stringResource(id = uiError.titleKey)) },
+                title = { Text(text = stringResource(id = R.string.forbidden_error_title)) },
                 text = {
-                    Text(text = uiError.messageKeys
-                        .mapIndexed { _, it -> stringResource(id = it) }
-                        .joinToString(" ")
-                    )
+                    Text(text = stringResource(id = R.string.forbidden_error_message))
+                },
+                confirmButton = { },
+                dismissButton = {
+                    Button(onClick = onDismiss) {
+                        Text(text = stringResource(id = android.R.string.cancel))
+                    }
+                }
+            )
+        }
+        is CountryListError.Other, CountryListError.UserNotLoggedIn -> {
+            AlertDialog(
+                onDismissRequest = onDismiss,
+                title = { Text(text = stringResource(id = R.string.forbidden_error_title)) },
+                text = {
+                    Text(text = stringResource(id = R.string.generic_error_message))
+                },
+                confirmButton = { },
+                dismissButton = {
+                    Button(onClick = onDismiss) {
+                        Text(text = stringResource(id = android.R.string.cancel))
+                    }
+                }
+            )
+        }
+        is CountryListError.ConnectionError -> {
+            AlertDialog(
+                onDismissRequest = onDismiss,
+                title = { Text(text = stringResource(id = R.string.connection_error_title)) },
+                text = {
+                    Text(text = stringResource(id = R.string.connection_error_message1))
                 },
                 confirmButton = { },
                 dismissButton = {
